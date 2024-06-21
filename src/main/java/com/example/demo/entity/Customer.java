fa6cde5e343faf1fb2828b84a6d01d57903e93a9
student <student@AP-0117>
2024-06-21 14:43:03 +0900

    管理者のユーザー情報取得

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,18 +20,19 @@ public class Customer {
 	private String address;
 	private String tel;
 	private String email;
-	private String birthday;
+	private LocalDate birthday;
 	@Column(name = "register_date")
-	private String registerDate;
+	private LocalDate registerDate;
 	@Column(name = "withdraw_date")
-	private String withdrawDate;
+	private LocalDate withdrawDate;
 	private String password;
 
 	public Customer() {
 	}
 
-	public Customer(Integer id, String name, String postal, String address, String tel, String email, String birthday,
-			String registerDate, String withdrawDate, String password) {
+	public Customer(Integer id, String name, String postal, String address, String tel, String email,
+			LocalDate birthday,
+			LocalDate registerDate, LocalDate withdrawDate, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -91,27 +94,27 @@ public class Customer {
 		this.email = email;
 	}
 
-	public String getBirthday() {
+	public LocalDate getBirthday() {
 		return birthday;
 	}
 
-	public void setBirthday(String birthday) {
+	public void setBirthday(LocalDate birthday) {
 		this.birthday = birthday;
 	}
 
-	public String getRegisterDate() {
+	public LocalDate getRegisterDate() {
 		return registerDate;
 	}
 
-	public void setRegisterDate(String registerDate) {
+	public void setRegisterDate(LocalDate registerDate) {
 		this.registerDate = registerDate;
 	}
 
-	public String getWithdrawDate() {
+	public LocalDate getWithdrawDate() {
 		return withdrawDate;
 	}
 
-	public void setWithdrawDate(String withdrawDate) {
+	public void setWithdrawDate(LocalDate withdrawDate) {
 		this.withdrawDate = withdrawDate;
 	}
 

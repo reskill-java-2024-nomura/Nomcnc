f309b5680b8358829171b73b12189c491c8adf00
student <student@AP-0117>
2024-06-21 10:48:13 +0900

    ＊＊＊

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 class HotelController {
 
 	@GetMapping("/top")
-	public String top() {
+	public String top(
+			@RequestParam("categoryId") Integer categoryId,
+			Model model) {
+		
+		
 		return "top";
 	}
+	
+	@GetMapping("/hotels")
+	
 }

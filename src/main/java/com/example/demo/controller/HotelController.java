d95e75685a09fa0a95423e617da63e4cfd476266
student <student@AP-0117>
2024-06-21 12:54:54 +0900

    category

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -1,22 +1,50 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Category;
+import com.example.demo.repository.CategoryRepository;
 
 @Controller
 class HotelController {
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
 	@GetMapping("/top")
 	public String top(
-			@RequestParam("categoryId") Integer categoryId,
 			Model model) {
-		
-		
+
+		List<Category> categories = categoryRepository.findAll();
+		model.addAttribute("categories", categories);
 		return "top";
 	}
-	
+
 	@GetMapping("/hotels")
-	
+	public String index(
+			@RequestParam(name = "categoryName", defaultValue = "") String categoryName,
+			Model model) {
+
+		List<Category> hotelList = categoryRepository.findByName(categoryName);
+		model.addAttribute("hotelList", hotelList);
+
+		return "hotelList";
+	}
+
+	@PostMapping("/hotels")
+	public String getCategory(
+			@RequestParam(name = "categoryName", defaultValue = "") String categoryName,
+			Model model) {
+		model.addAttribute("categoryName", categoryName);
+
+		return "hotelList";
+	}
+
 }

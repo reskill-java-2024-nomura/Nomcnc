758374cd093d358e57aac38dba77472d84196de3
student <student@AP-0117>
2024-06-21 16:26:10 +0900

    addplanの変更2

--- src/main/java/com/example/demo/controller/AdminPlanController.java
+++ src/main/java/com/example/demo/controller/AdminPlanController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminPlanController {
@@ -13,7 +15,10 @@ public class AdminPlanController {
 	}
 
 	@PostMapping("/admin/plans/add")
-	public String store() {
+	public String store(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			Model model) {
+		model.addAttribute("name", name);
 		return "adminshowhotels";
 	}
 }
